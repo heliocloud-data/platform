536e057a32ff67b0d09b161f7a99114fad408866
Vicki Toy-Edens <vicki.toy-edens@jhuapl.edu>
2023-04-12 13:32:27 -0400

    Incorporate A&A into daskhub scripts
    - Move Cognito Daskhub component into Daskhub stack
    - Move copy of Cognito app setup into Dashboard stack
    - Move S3 IAM policy and bucket creation to base aws stack
    - Simplify moving files to EC2 instance
    - Fix permissions on start up files on EC2 instance (first SSM user not created until log-in)
    - Use latest amazon linux image instead of default one which was deprecated in 2020
    - Pass A&A info through Cloudformation outputs
    - Add main config to daskhub
    - Rename shell scripts
    - Fix EFS calls so they remove mounted targets if outside of VPN (otherwise can not mount new ones)
    - New script to deploy daskhub helm chart
    - Bash script takes care of auth setup
    - Deploy helm chart in 2 stages and grabbing loadbalancer URL to update Route53
    - Update EKS config to use newer parameter names
    - Update README to remove now automated steps (still needs to be updated for debugging and deleting Daskhub)
    - Not included is ability to configure heliocloud docker container

--- install/base_aws/base_aws_stack.py
+++ install/base_aws/base_aws_stack.py
@@ -6,7 +6,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_ec2 as ec2,
     aws_kms as kms,
-    custom_resources as resources,
+    aws_iam as iam,
 )
 from constructs import Construct
 
@@ -24,9 +24,61 @@ class BaseAwsStack(Stack):
         with open(config, 'r') as file:
             configuration = yaml.safe_load(file)
 
+        registry = configuration['registry']
+        public_buckets = registry.get('bucketNames')
+
         # TODO:  What do we need to put in here?
         # Create own VPC for HelioCloud
         self.heliocloud_vpc = ec2.Vpc(self, "HelioCloudVPC")
 
         self.kms = kms.Key(self, "HelioCloudKMS")
         self.kms.add_alias('heliocloud')
+
+        ###############################################
+        # Create S3 Bucket for shared user storage #
+        ###############################################
+        # TODO potentially make this configurable along with component in s3 policy document
+        # TODO figure out retention plan, persists after stack deleted
+        user_shared_bucket = s3.Bucket(self, "UserSharedBucket")
+
+        # TODO programmatically add additional policy statements
+        # based on known HelioCloud public buckets (maybe use user script to pull names?)
+        # Need to iteratively adjust though, maybe lambda
+        other_known_public_buckets = ['helio-public']
+        public_bucket_arns = []
+        for public_bucket in public_buckets + other_known_public_buckets:
+            public_bucket_arns += [f"arn:aws:s3:::{public_bucket}",
+                                   f"arn:aws:s3:::{public_bucket}/*"]
+
+        s3_custom_policy_document = iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    actions=["s3:PutObject",
+                             "s3:GetObject",
+                             "s3:ListBucketMultipartUploads",
+                             "s3:AbortMultipartUpload",
+                             "s3:ListBucketVersions",
+                             "s3:CreateBucket",
+                             "s3:ListBucket",
+                             "s3:DeleteObject",
+                             "s3:GetBucketLocation",
+                             "s3:ListMultipartUploadParts"],
+                    resources=[user_shared_bucket.bucket_arn,
+                               f"{user_shared_bucket.bucket_arn}/*"]
+                ),
+                iam.PolicyStatement(
+                    actions=["s3:ListAllMyBuckets"],
+                    resources=["*"]
+                ),
+                iam.PolicyStatement(
+                    actions=["s3:GetObject",
+                             "s3:ListBucketVersions",
+                             "s3:ListBucket",
+                             "s3:GetBucketLocation"],
+                    resources=public_bucket_arns
+                )
+            ]
+        )
+
+        self.s3_managed_policy = iam.ManagedPolicy(self, "S3ManagedPolicy",
+                                              document=s3_custom_policy_document)
--- install/daskhub/daskhub_stack.py
+++ install/daskhub/daskhub_stack.py
@@ -3,11 +3,11 @@ import os
 import aws_cdk as cdk
 from aws_cdk import (
     Stack,
-    aws_s3 as s3,
     aws_iam as iam,
     aws_ec2 as ec2,
     aws_efs as efs,
     aws_s3_assets as s3_assets,
+    aws_cognito as cognito,
 )
 from constructs import Construct
 
@@ -17,16 +17,13 @@ class DaskhubStack(Stack):
     CDK stack for installing DaskHub for a HelioCloud instance
     """
 
-    def __init__(self, scope: Construct, construct_id: str, base_stack, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, base_aws, base_auth, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # get the configuration file from the context
         config = self.node.try_get_context("config")
         with open(config, "r") as file:
             configuration = yaml.safe_load(file)
-
-        registry = configuration['registry']
-        public_buckets = registry.get('bucketNames')
 
         #############################
         # Create EC2 Admin instance #
@@ -55,107 +52,52 @@ class DaskhubStack(Stack):
         SSM_AGENT_RPM = 'https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm'
         ec2_user_data = ec2.UserData.for_linux()
 
-        
-        with open('daskhub/deploy/01-tools.sh', encoding='UTF-8') as file:
+        with open('daskhub/deploy/00-tools.sh', encoding='UTF-8') as file:
             bash_lines = file.read()
 
-        # TODO remove all of these when scripts are available on open git repo
-        # that requires no auth, instead add git commands to 01-tools.sh
+        init_file_args = []
         deploy_dir = 'daskhub/deploy'
-        config_deploy_file_list = [f for f in os.listdir(deploy_dir) if os.path.isfile(os.path.join(deploy_dir, f)) and f.split(".")[-1] != 'sh']
-        assets_tuple_list = [(f, s3_assets.Asset(self, f'K8sAssets{i}', path=os.path.join(os.getcwd(), f'daskhub/deploy/{f}'))) for i, f in enumerate(config_deploy_file_list)]
+        config_deploy_file_list = [f for f in os.listdir(
+            deploy_dir) if os.path.isfile(os.path.join(deploy_dir, f))]
+        for i, f in enumerate(config_deploy_file_list):
+            if f.split(".")[-1] == 'sh':
+                mode = '000777'  # All levels read/write/execute
+            else:
+                mode = '000666'  # All levels read/write
+            init_file_args.append(ec2.InitFile.from_existing_asset(f"/home/ssm-user/{f}",
+                                                                   s3_assets.Asset(self, f'K8sAssets{i}', path=os.path.join(
+                                                                       os.getcwd(), f'daskhub/deploy/{f}')),
+                                                                   owner='ssm-user',
+                                                                   mode=mode))
 
-        shell_script_deploy_file_list = [f for f in os.listdir(deploy_dir) if os.path.isfile(os.path.join(deploy_dir, f)) and f.split(".")[-1] == 'sh']
-        assets_sh_tuple_list = [(f, s3_assets.Asset(self, f'K8sAssetsSh{i}', path=os.path.join(os.getcwd(), f'daskhub/deploy/{f}'))) for i, f in enumerate(shell_script_deploy_file_list)]
+        init_data = ec2.CloudFormationInit.from_elements(*init_file_args)
 
-        # TODO find a way to bundle and unbundle these files with correct permissions to avoid
-        # having to move them like this
-        init_data = ec2.CloudFormationInit.from_elements(
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[0][0]}", assets_tuple_list[0][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[1][0]}", assets_tuple_list[1][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[2][0]}", assets_tuple_list[2][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[3][0]}", assets_tuple_list[3][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[4][0]}", assets_tuple_list[4][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[5][0]}", assets_tuple_list[5][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_tuple_list[6][0]}", assets_tuple_list[6][1], mode='000666'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_sh_tuple_list[0][0]}", assets_sh_tuple_list[0][1], mode='000777'),
-                                                         ec2.InitFile.from_existing_asset(f"/home/ssm-user/{assets_sh_tuple_list[1][0]}", assets_sh_tuple_list[1][1], mode='000777')
-                                                         )
-
-        # TODO figure out why chown statement won't work in either the cloudformationinit or user data
         ec2_user_data.add_commands('sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm',
                                    'sudo yum -y install jq gettext bash-completion moreutils docker unzip nano git',
                                    f'sudo yum install -y {SSM_AGENT_RPM}',
                                    'restart amazon-ssm-agent',
-                                    bash_lines,
-                                    'sudo chown -R ssm-user:ssm-user /home/ssm-user'  #TODO figure out why this doesn't work
-                                    )
+                                   'adduser -m ssm-user',
+                                   'echo "ssm-user ALL=(ALL) NOPASSWD:ALL" | tee  /etc/sudoers.d/ssm-agent-users',
+                                   'chmod 440 /etc/sudoers.d/ssm-agent-users ',
+                                   bash_lines,
+                                   'sudo chown -R ssm-user:ssm-user /home/ssm-user'
+                                   )
 
         # TODO: add cloudwatch logs to SSM
         # Create admin instance and attach role
         instance = ec2.Instance(self, "DaskhubInstance",
-                                vpc=base_stack.heliocloud_vpc,
-                                machine_image=ec2.AmazonLinuxImage(),
+                                vpc=base_aws.heliocloud_vpc,
+                                machine_image=ec2.MachineImage.latest_amazon_linux(),
                                 instance_type=ec2.InstanceType('t2.micro'),
                                 role=ec2_admin_role,
                                 user_data=ec2_user_data,
                                 vpc_subnets=ec2.SubnetSelection(
-                                    subnets=base_stack.heliocloud_vpc.private_subnets),
+                                    subnets=base_aws.heliocloud_vpc.private_subnets),
                                 init=init_data)
-
-        ###############################################
-        # Create S3 Bucket for shared Daskhub storage #
-        ###############################################
-        # TODO potentially make this configurable along with component in s3 policy document
-        # TODO figure out retention plan, persists after stack deleted
-        # TODO make this an option (ALSO MUST MAKE OPTIONAL IN K8s SCRIPTS)
-        daskhub_bucket = s3.Bucket(self, "DaskhubBucket")
 
         ####################################################
         # Managed Policies (S3 access and K8s autoscaling) #
         ####################################################
-
-        # TODO programmatically add additional policy statements
-        # based on known HelioCloud public buckets (maybe use user script to pull names?)
-        # Need to iteratively adjust though, maybe lambda
-        other_known_public_buckets = ['helio-public']
-        public_bucket_arns = []
-        for public_bucket in public_buckets + other_known_public_buckets:
-            public_bucket_arns += [f"arn:aws:s3:::{public_bucket}",
-                                   f"arn:aws:s3:::{public_bucket}/*"]
-
-        s3_custom_policy_document = iam.PolicyDocument(
-            statements=[
-                iam.PolicyStatement(
-                    actions=["s3:PutObject",
-                             "s3:GetObject",
-                             "s3:ListBucketMultipartUploads",
-                             "s3:AbortMultipartUpload",
-                             "s3:ListBucketVersions",
-                             "s3:CreateBucket",
-                             "s3:ListBucket",
-                             "s3:DeleteObject",
-                             "s3:GetBucketLocation",
-                             "s3:ListMultipartUploadParts"],
-                    resources=[daskhub_bucket.bucket_arn,
-                               f"{daskhub_bucket.bucket_arn}/*"]
-                ),
-                iam.PolicyStatement(
-                    actions=["s3:ListAllMyBuckets"],
-                    resources=["*"]
-                ),
-                iam.PolicyStatement(
-                    actions=["s3:GetObject",
-                             "s3:ListBucketVersions",
-                             "s3:ListBucket",
-                             "s3:GetBucketLocation"],
-                    resources=public_bucket_arns
-                )
-            ]
-        )
-
-        s3_managed_policy = iam.ManagedPolicy(self, "S3ManagedPolicy",
-                                              document=s3_custom_policy_document)
 
         autoscaling_custom_policy_document = iam.PolicyDocument(
             statements=[
@@ -179,10 +121,33 @@ class DaskhubStack(Stack):
         # TODO figure out retention plan, persists after stack deleted
         # TODO make this an option (ALSO MUST MAKE OPTIONAL IN K8s SCRIPTS)
         file_system = efs.FileSystem(self, "DaskhubEFS",
-                                     vpc=base_stack.heliocloud_vpc,
+                                     vpc=base_aws.heliocloud_vpc,
                                      encrypted=True,
                                      enable_automatic_backups=True
                                      )
+
+        ##############################################
+        # Authentication and Authorization (Cognito) #
+        ##############################################
+        daskhub_client = base_auth.userpool.add_client("heliocloud-daskhub",
+                                                       generate_secret=True,
+                                                       o_auth=cognito.OAuthSettings(
+                                                           flows=cognito.OAuthFlows(
+                                                               authorization_code_grant=True),
+                                                           scopes=[cognito.OAuthScope.PHONE,
+                                                                   cognito.OAuthScope.EMAIL,
+                                                                   cognito.OAuthScope.OPENID,
+                                                                   cognito.OAuthScope.COGNITO_ADMIN,
+                                                                   cognito.OAuthScope.PROFILE],
+                                                           callback_urls=[
+                                                               'https://example.com/hub/oauth_callback'],
+                                                           logout_urls=['https://example.com/logout']),
+                                                       supported_identity_providers=[
+                                                           cognito.UserPoolClientIdentityProvider.COGNITO],
+                                                       prevent_user_existence_errors=True)
+        daskhub_client_id = daskhub_client.user_pool_client_id
+        auth = configuration['auth']
+        domain_prefix = auth.get('domain_prefix', '')
 
         ##########################
         # CloudFormation Outputs #
@@ -194,13 +159,16 @@ class DaskhubStack(Stack):
         cdk.CfnOutput(self, 'ASGArn',
                       value=autoscaling_managed_policy.managed_policy_arn)
         cdk.CfnOutput(self, 'KMSArn',
-                      value=base_stack.kms.key_arn)
+                      value=base_aws.kms.key_arn)
         cdk.CfnOutput(self, 'CustomS3Arn',
-                      value=s3_managed_policy.managed_policy_arn)
+                      value=base_aws.s3_managed_policy.managed_policy_arn)
         cdk.CfnOutput(self, 'AdminRoleArn',
-                      value=ec2_admin_role.role_arn)  
+                      value=ec2_admin_role.role_arn)
         cdk.CfnOutput(self, 'EFSId',
                       value=file_system.file_system_id)
-        cdk.CfnOutput(self, 'AssetBucket',
-                      value=assets_tuple_list[0][1].s3_bucket_name)
-        
+        cdk.CfnOutput(self, 'CognitoClientId',
+                      value=daskhub_client_id)
+        cdk.CfnOutput(self, 'CognitoDomainPrefix',
+                      value=domain_prefix)
+        cdk.CfnOutput(self, 'CognitoUserPoolId',
+                      value=base_auth.userpool.user_pool_id)

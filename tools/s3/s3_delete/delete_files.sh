bb04f3cebde3ce3849c82ad6a9377dcf6a7255f7
Brian Thomas <brian.a.thomas@nasa.gov>
2024-11-18 22:00:37 +0000

    Resolve "Clean up S3 Delete scripts"

--- tools/s3/s3_delete/delete_files.sh
+++ tools/s3/s3_delete/delete_files.sh
@@ -1,12 +1,12 @@
 
-BUCKET=$0
+BUCKET_NAME=$0
 OUTPUT_LIST=$1
 
 # 1. get a list of all current object on bucket
-python make_objlist.py -b $BUCKET -a -o $OUTPUT_LIST
+# # you can use an output of the s3_comparison find_missing_files_in_dest.sh script
 
 # 2. Create a list of shell commands to execute
-cmds=`python make_delete_script.py -b helio-data-staging -o $OUTPUT_LIST`
+cmds=`python make_delete_script.py -b $BUCKET_NAME -o $OUTPUT_LIST`
 echo $cmds
 
 # 3. Execute commands to delete data

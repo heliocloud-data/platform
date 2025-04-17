0c3decba5d29d66e174212a8a6879b98293c47e1
Nicholas Lenzi <Nicholas.Lenzi@jhuapl.edu>
2025-04-17 12:50:04 -0400

    heliocloud/platform#265: Update nodejs version from 18 to 22.

--- scripts/install-deps-nodejs-no_sudo.sh
+++ scripts/install-deps-nodejs-no_sudo.sh
@@ -8,7 +8,7 @@
 # From: https://github.com/nodesource/distributions
 apt-get update &&  apt-get install -y ca-certificates curl gnupg && mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-export NODE_MAJOR=18 && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+export NODE_MAJOR=22 && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update && apt-get install -y nodejs
 
 node --version
--- scripts/install-deps-nodejs.sh
+++ scripts/install-deps-nodejs.sh
@@ -9,7 +9,7 @@
 # From: https://github.com/nodesource/distributions
 sudo apt-get update &&  sudo apt-get install -y ca-certificates curl gnupg && sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
-export NODE_MAJOR=18 && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+export NODE_MAJOR=22 && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install -y nodejs
 
 node --version

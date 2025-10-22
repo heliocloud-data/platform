a58cf1f5a06878e86a7d34715ef4b30f2cf288e4
Shumate, Peter F <Peter.Shumate@jhuapl.edu>
2025-10-22 10:20:55 -0500

    Add ingress installation

---
+++ daskhub/deploy/02-deploy-ingress.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is a mandatory step to ensure deployment of ingress/authentication proxies
+
+cd ../ingress
+helm dep update
+helm upgrade \
+    heliocloud-ingress ./ \
+    --create-namespace \
+    --namespace ingress \
+    --values=values.yaml \
+    --values=values-production.yaml \
+    --install --timeout 10m30s --debug

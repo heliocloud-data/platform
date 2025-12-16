5d09c7fc996b993ef46acfce9a707f4fd30b08f9
Sandy Antunes <sandy.antunes@jhuapl.edu>
2025-12-16 15:04:57 -0500

    fixed black error

--- portal/portal_stack.py
+++ portal/portal_stack.py
@@ -61,7 +61,11 @@ class PortalStack(Stack):
             user_pool=auth_stack.userpool, user_pool_client=auth_stack.client
         )
 
-        vpc=aws_stack.heliocloud_vpc() if callable(aws_stack.heliocloud_vpc) else aws_stack.heliocloud_vpc,
+        vpc = (
+            aws_stack.heliocloud_vpc()
+            if callable(aws_stack.heliocloud_vpc)
+            else aws_stack.heliocloud_vpc
+        )
         # Create the Portal task for Fargate
         task = self.__create_ec2_resources(
             vpc=vpc,
@@ -79,9 +83,7 @@ class PortalStack(Stack):
         cdk.CfnOutput(
             self, "Portal_Ec2InstanceProfile", value=self.ec2_default_instance_profile.attr_arn
         )
-        cdk.CfnOutput(
-            self, "Portal_Ec2SubnetId", value=vpc.public_subnets[0].subnet_id
-        )
+        cdk.CfnOutput(self, "Portal_Ec2SubnetId", value=vpc.public_subnets[0].subnet_id)
         cdk.CfnOutput(self, "Portal_Ec2RoleArn", value=self.ec2_default_role.role_arn)
         cdk.CfnOutput(self, "Portal_IdentityPool", value=id_pool.identity_pool_id)
 

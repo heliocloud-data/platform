02a77e25fe4175c6c10385c074f1037105384d22
shumatepf <shumatepfs@gmail.com>
2026-05-20 11:24:41 -0400

    Setup new dev env

--- modules/heliocloud_eks_addon_pod_identity/main.tf
+++ modules/heliocloud_eks_addon_pod_identity/main.tf
@@ -1,9 +1,16 @@
 # The Pod Identity Agent must be installed on each node
 
 # It handles credential injection for pods
+data "aws_eks_addon_version" "pod_identity" {
+  addon_name         = "eks-pod-identity-agent"
+  kubernetes_version = var.kubernetes_version
+  most_recent        = true
+}
+
 resource "aws_eks_addon" "pod_identity" {
   cluster_name                = var.cluster_name
   addon_name                  = "eks-pod-identity-agent"
+  addon_version               = data.aws_eks_addon_version.pod_identity.version
   resolve_conflicts_on_update = "OVERWRITE"
 
   tags = { Name = "pod-identity-agent" }

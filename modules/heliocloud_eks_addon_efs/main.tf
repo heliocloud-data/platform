1161bb3a3f1a74eef59887d53764209b352de486
Nicholas Lenzi <Nicholas.Lenzi@jhuapl.edu>
2026-06-09 11:26:48 -0400

    platform#49: EFS Mount not working in multi-az configuration

--- modules/heliocloud_eks_addon_efs/main.tf
+++ modules/heliocloud_eks_addon_efs/main.tf
@@ -1,29 +1,42 @@
-resource "aws_eks_addon" "efs" {
-  cluster_name                = var.cluster_name
-  addon_name                  = "eks-efs-csi-driver"
-  addon_version               = "v3.0.1-eksbuild.1"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  pod_identity_association {
-    role_arn        = aws_iam_role.HelioCloud_efs-csi-driver_ServiceAccount.arn
-    service_account = var.kubernetes_service_account
-  }
-
-  tags = { Name = "eks-efs-csi-driver" }
-}
+data "aws_region" "current" {}
 
 locals {
+  name                     = "aws-efs-csi-driver"
   iam_policy_document_json = file("${path.module}/iam_policy_document.json")
+
+  qualifier = var.qualifier != "" ? var.qualifier : "${var.cluster_name}"
 }
 
-resource "aws_iam_policy" "aws-efs-csi-driver_policy" {
-  name   = "HelioCloud_${var.cluster_name}_EfsMountManagedPolicy"
+data "aws_eks_addon_version" "aws-efs-csi-driver" {
+  addon_name         = local.name
+  kubernetes_version = var.kubernetes_version
+  most_recent        = true
+
+  # depends_on = [
+  # aws_iam_role_policy_attachment.nodegroup_AmazonEBSCSIDriverPolicy_ServiceAccount,
+  # aws_iam_role_policy_attachment.nodegroup_AmazonEBSCSIDriverPolicyV2_ServiceAccount
+  # ]
+}
+resource "aws_eks_addon" "efs" {
+  cluster_name                = var.cluster_name
+  addon_name                  = local.name
+  addon_version               = data.aws_eks_addon_version.aws-efs-csi-driver.version
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  tags = { Name = local.name }
+}
+
+# pod_identity_association {
+#   role_arn        = aws_iam_role.HelioCloud_efs-csi-driver_ServiceAccount.arn
+#   service_account = var.kubernetes_service_account
+# }
+
+# tags = { Name = "eks-efs-csi-driver" }
+# }
+
+resource "aws_iam_policy" "HelioCloud_EfsMountManagedPolicy" {
+  name   = "HelioCloud_${local.qualifier}_EfsMountManagedPolicy"
   policy = local.iam_policy_document_json
-}
-
-resource "aws_iam_role_policy_attachment" "aws-efs-csi-driver_policy_attachment" {
-  policy_arn = aws_iam_policy.aws-efs-csi-driver_policy.arn
-  role       = var.worker_node_role_name
 }
 
 data "aws_iam_policy_document" "pod_assume_role" {
@@ -42,12 +55,35 @@ data "aws_iam_policy_document" "pod_assume_role" {
   }
 }
 
-resource "aws_iam_role" "HelioCloud_efs-csi-driver_ServiceAccount" {
-  name               = "HelioCloud_${var.cluster_name}_efs-csi-driver_ServiceAccount"
+# TODO SECURITY GROUP for worker nodes to EFS target...
+resource "aws_iam_role_policy_attachment" "HelioCloud_WorkerNodeRole_AmazonEFSCSIDriverPolicy_PolicyAttachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+  role       = var.worker_node_role_name
+}
+
+resource "aws_iam_role_policy_attachment" "HelioCloud_WorkerNodeRole_EfsMountManagedPolicy_PolicyAttachment" {
+  policy_arn = aws_iam_policy.HelioCloud_EfsMountManagedPolicy.arn
+  role       = var.worker_node_role_name
+}
+
+resource "aws_iam_role" "HelioCloud_EFSCSIDriver_ServiceAccount" {
+  name               = substr("HelioCloud_${local.qualifier}_EFSCSIDriver_ServiceAccount", 0, 64)
   assume_role_policy = data.aws_iam_policy_document.pod_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "HelioCloud_efs-csi-driver_ServiceAccount" {
-  policy_arn = aws_iam_policy.aws-efs-csi-driver_policy.arn
-  role       = aws_iam_role.HelioCloud_efs-csi-driver_ServiceAccount.name
+resource "aws_iam_role_policy_attachment" "HelioCloud_EFSCSIDriver_AmazonEFSCSIDriverPolicy_PolicyAttachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+  role       = aws_iam_role.HelioCloud_EFSCSIDriver_ServiceAccount.name
+}
+
+resource "aws_iam_role_policy_attachment" "HelioCloud_EFSCSIDriver_EfsMountManagedPolicy_PolicyAttachment" {
+  policy_arn = aws_iam_policy.HelioCloud_EfsMountManagedPolicy.arn
+  role       = aws_iam_role.HelioCloud_EFSCSIDriver_ServiceAccount.name
+}
+
+resource "aws_eks_pod_identity_association" "HelioCloud_EFSCSIDriver_PodIdentityAssociation" {
+  cluster_name    = var.cluster_name
+  namespace       = var.kubernetes_namespace
+  service_account = var.kubernetes_service_account
+  role_arn        = aws_iam_role.HelioCloud_EFSCSIDriver_ServiceAccount.arn
 }
